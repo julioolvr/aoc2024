203474ab0c6e1fc09250f91ab96b9f28e0d5467f
Julio Olivera <julio.olvr@gmail.com>
2024-12-01 21:16:59 +0000

    Day 01 Part 2

--- day01/src/main.rs
+++ day01/src/main.rs
@@ -1,4 +1,9 @@
-use std::{env, fs::File, io::{self, BufRead}};
+use std::{
+    collections::HashMap,
+    env,
+    fs::File,
+    io::{self, BufRead},
+};
 
 fn main() {
     let lines = read_lines().expect("Unable to read lines");
@@ -8,9 +13,10 @@ fn main() {
 
     for line in lines {
         let location_ids: Vec<usize> = line
-        .unwrap()
-        .split_whitespace()
-        .map(|n| n.parse::<usize>().unwrap()).collect();
+            .unwrap()
+            .split_whitespace()
+            .map(|n| n.parse::<usize>().unwrap())
+            .collect();
 
         first_location_ids.push(location_ids[0]);
         second_location_ids.push(location_ids[1]);
@@ -19,10 +25,25 @@ fn main() {
     first_location_ids.sort();
     second_location_ids.sort();
 
-    let distances = first_location_ids.iter().zip(second_location_ids.iter()).map(|(first, second)| usize::abs_diff(*first, *second));
+    let mut second_locations_map: HashMap<usize, usize> = HashMap::new();
+
+    for location_id in &second_location_ids {
+        *second_locations_map.entry(*location_id).or_insert(0) += 1;
+    }
+
+    let distances = first_location_ids
+        .iter()
+        .zip(second_location_ids.iter())
+        .map(|(first, second)| usize::abs_diff(*first, *second));
     let part_1: usize = distances.sum();
 
     println!("Part 1 {}", part_1);
+
+    let part_2: usize = first_location_ids
+        .iter()
+        .map(|location_id| second_locations_map.get(location_id).unwrap_or(&0) * location_id)
+        .sum();
+    println!("Part 2 {}", part_2);
 }
 
 fn read_lines() -> io::Result<io::Lines<io::BufReader<File>>> {
